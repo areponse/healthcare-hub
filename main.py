a33b1df34ae504ee53d61479bd0ac8f1f20d43ce
divinekaremera <145830581+divinekaremera@users.noreply.github.com>
2023-12-08 00:14:02 +0200

    Add functionality

--- main.py
+++ main.py
@@ -13,3 +13,23 @@ class healthcare_hub:
         # Create a cursor object
         self.cursor = self.connection.cursor()
 
+def execute_query(self, query, data=None, fetch=False):
+        self.cursor.execute(query, data)
+        if fetch:
+            return self.cursor.fetchall()
+        else:
+            self.connection.commit()
+
+    def get_welcome_message(self):
+        return "Welcome to Healthcare Hub! Stay healthy and informed.\n"
+
+    def display_menu(self):
+        print("\nHealthcare Hub Console Application\n")
+        print("1. Disease Information")
+        print("2. Symptom Exploration")
+        print("3. Drug Exploration")
+        print("4. Educational Flashcards")
+        print("5. Random Health Tip")
+        print("6. Posts Exploration")
+        print("7. Share Post")
+        print("8. Exit\n")
