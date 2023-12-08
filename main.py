464c6a38b96889902aad0ddbc3298cb875aa0fca
Ctuyishi <144099125+Ctuyishi@users.noreply.github.com>
2023-12-08 08:19:56 +0200

    health

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ class healthcare_hub:
         # Create a cursor object
         self.cursor = self.connection.cursor()
 
- if results:
-        for result in results:
+  for result in results:
             print(result)
     else:
-        print("No flashcards found for the specified category.")
+        print(f"No {category}s found.")
